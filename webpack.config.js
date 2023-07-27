f5a184920f810e4ec6c5122c71193887c8dd88d6
Neil Murphy <barrymun@tcd.ie>
2023-07-27 22:21:36 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+    return {
+        mode: argv.mode === 'development' ? 'development' : 'production',
+        devtool: argv.mode === 'development' ? 'cheap-module-source-map' : 'source-map',
+        entry: {
+            main: [
+                './src/index.ts',
+            ],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devtool: 'source-map',
+        plugins: [
+            new CleanWebpackPlugin(),
+            new CopyPlugin({
+                patterns: [{ from: 'src/index.html', to: 'index.html' }],
+            }),
+        ],
+    };
+};
