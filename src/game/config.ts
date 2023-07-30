4d0b6a61db3800c63cd78c48be695107d071b8a7
Neil Murphy <barrymun@tcd.ie>
2023-07-30 13:16:23 +0100

    Config class

--- src/game/config.ts
+++ src/game/config.ts
@@ -1,5 +1,23 @@
 export class Config {
+    public canvas: HTMLCanvasElement;
+
+    public getCanvas = (): HTMLCanvasElement => this.canvas;
+
+    private setCanvas = (canvas: HTMLCanvasElement): void => {
+        this.canvas = canvas;
+    };
+    
+    public context: CanvasRenderingContext2D;
+    
+    public getContext = (): CanvasRenderingContext2D => this.context;
+
+    private setContext = (context: CanvasRenderingContext2D): void => {
+        this.context = context;
+    };
+    
     constructor() {
+        this.setCanvas(document.getElementById('c')! as HTMLCanvasElement);
+        this.setContext(this.canvas.getContext('2d')!);
         console.log('Config constructor');
     }
 };
